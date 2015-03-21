5ffd0169ae0f3abdaef009306d78b7d93920187b
Fabien Parent <parent.f@gmail.com>
2015-03-21 18:43:10 +0100

    apps: free: add heap usage + overall mem usage

--- apps/free.c
+++ apps/free.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
 
 #include <kernel/shell.h>
 
@@ -15,11 +16,19 @@ int free_main(int argc, char **argv)
 {
     extern uint32_t _sheap;
     extern uint32_t _eor;
+    extern uint32_t _bor;
     uint32_t heap_pos = _sbrk(0);
 
-    printf("Memory:");
-    printf("\ttotal: unknown\n");
-    printf("\tused: unknown\n");
+    printf("Memory:\n");
+    printf("\ttotal: %u\n", (uint32_t) &_eor - (uint32_t) &_bor);
+    printf("\tused: %u\n", heap_pos - (uint32_t) &_bor);
+    printf("\tfree: %u\n", (uint32_t) &_eor - heap_pos);
+
+    printf("\n");
+
+    printf("Heap:\n");
+    printf("\ttotal: %u\n", (uint32_t) &_eor - (uint32_t) &_sheap);
+    printf("\tused: %u\n", heap_pos - (uint32_t) &_sheap);
     printf("\tfree: %u\n", (uint32_t) &_eor - heap_pos);
 
     return 0;
